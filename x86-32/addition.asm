cfb9210ff0e3f3274aacdd43c905ed921d54ed41
patangraoamol03-art <patangrao.amol03@gmail.com>
2026-01-20 22:14:25 +0530

    Create addition.asm

---
+++ x86-32/addition.asm
@@ -0,0 +1,25 @@
+section .data
+    a dd 10
+    b dd 5
+    msg db "Addition of %d and %d is %d",10,0
+
+section .bss
+    c resd 1
+
+section .text
+   global main
+   extern printf
+
+main:
+   mov eax,dword[a]
+   mov ebx,dword[b]
+   add eax, ebx
+   mov dword[c],eax
+
+   push dword[c]
+   push dword[b]
+   push dword[a]
+   push msg
+   call printf
+   add esp,16
+   ret
